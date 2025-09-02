37edde6fd47d422ca45738d1ecb8c61c49454a31
Gokulashik M <119474711+Gokulashik22014@users.noreply.github.com>
2025-09-02 23:00:39 +0530

    something is going wrong

--- bridge/Main.java
+++ bridge/Main.java
@@ -1,7 +1,7 @@
-package adapterp;
+package bridge;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
