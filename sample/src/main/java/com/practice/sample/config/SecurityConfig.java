b8964983ed593cb1a0e420cda0439edebd1e0604
Gokulashik M <119474711+Gokulashik22014@users.noreply.github.com>
2025-09-09 17:59:56 +0530

    its hard

--- sample/src/main/java/com/practice/sample/config/SecurityConfig.java
+++ sample/src/main/java/com/practice/sample/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,20 +15,15 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
-	public UserDetailsService userDetails;
-	
-	@Autowired
-	public SecurityConfig(UserDetailsService userDetails) {
-		this.userDetails=userDetails;
-	}
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,25 +34,5 @@ public class SecurityConfig {
 				.build();
 	}
 	
-//	@Bean
-//	public UserDetailsService addUsersFromMemory() {
-//		UserDetails user1=User.withDefaultPasswordEncoder().username("goku").password("1234").build();
-//		return new InMemoryUserDetailsManager(user1);
-//	}
 	
-	@Bean 
-	public AuthenticationProvider authProvider() {
-		System.out.print("I am");
-		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-		provider.setUserDetailsService(userDetails);
-		return provider;
-	}
-	
-	 @Bean
-	    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-	        return http.getSharedObject(AuthenticationManagerBuilder.class)
-	                   .authenticationProvider(authProvider())
-	                   .build();
-	    }
 }
